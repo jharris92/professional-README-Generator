6cbaae33a7d3c2b10c9533fd63c1cdd36057feb4
John Harris <john.harris7292@gmail.com>
2021-12-06 14:15:05 -0500

    added questions

--- index.js
+++ index.js
@@ -4,3 +4,33 @@ const util = require('util');
 const emailValidator = require('email-validator');
 
 const generateMarkdown = require('.utils/generateMarkdown');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter your project title'}
+        },
+    },
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'What is the repository name of your project?',
+        validate: (value) => {
+            if (value) {return true} else { return 'Please enter your project title.'}
+        },
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+        validate: (value) => {
+            if (value) { return true } else { return 'Please enter description.'}
+        },
+    },
+    {
+        
+    }
+]
